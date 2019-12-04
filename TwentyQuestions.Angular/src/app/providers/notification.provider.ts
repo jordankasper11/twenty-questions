ac417d84d26fd958ba406b25a087652a335bfe1f
Jordan Kasper <jordankasper11@gmail.com>
2019-12-03 23:50:52 -0500

    SignalR reconnect

--- TwentyQuestions.Angular/src/app/providers/notification.provider.ts
+++ TwentyQuestions.Angular/src/app/providers/notification.provider.ts
@@ -4,23 +4,48 @@ import { environment } from '@environments';
 
 @Injectable()
 export class NotificationProvider {
+    friendsUpdated = new EventEmitter();
+    gamesListUpdated = new EventEmitter();
     gameUpdated = new EventEmitter<string>();
 
     connection: HubConnection;
 
     constructor() {
-        this.connect();
+        this.createConnection();
+    }
+
+    async createConnection(): Promise<void> {
+        const accessToken = localStorage['accessToken'];
+
+        if (accessToken) {
+            this.connection = new HubConnectionBuilder()
+                .withUrl(`${environment.requestUrlPrefix}/hubs/notifications`, { accessTokenFactory: () => accessToken })
+                .withAutomaticReconnect({
+                    nextRetryDelayInMilliseconds: retryContext => {
+                        if (retryContext.elapsedMilliseconds < 10 * 60 * 1000)
+                            return 10 * 1000;
+                        else if (retryContext.elapsedMilliseconds < 60 * 60 * 24 * 1000)
+                            return 60 * 1000;
+
+                        return null;
+                    }
+                })
+                .build();
+
+            this.connection.on('UpdateGame', (gameId: string) => {
+                this.gameUpdated.emit(gameId);
+            });
+
+            await this.connect();
+        }
     }
 
     async connect(): Promise<void> {
-        const accessToken = localStorage['accessToken'];
-
-        this.connection = new HubConnectionBuilder().withUrl(`${environment.requestUrlPrefix}/hubs/notifications`, { accessTokenFactory: () => accessToken }).build();
-
-        await this.connection.start();
-
-        this.connection.on('UpdateGame', (gameId: string) => {
-            this.gameUpdated.emit(gameId);
-        });
+        try {
+            await this.connection.start();
+        }
+        catch{
+            setTimeout(async () => await this.connect(), 10 * 1000);
+        }
     }
 }
